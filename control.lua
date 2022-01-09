f4eb3638d3f1693ea390e9637ac05fcb6ccf62a5
ZwerOxotnik <zweroxotnik@gmail.com>
2022-01-09 10:45:58 +0300

    Update to 0.5.9

--- control.lua
+++ control.lua
@@ -1,8 +1,14 @@
-local handler = require("event_handler")
 local mod_commands = require("mod-commands")
 local commands_control = require("commands-control")
 
 -- Adds commands
 mod_commands.handle_custom_commands(commands_control)
 
-handler.add_libraries({mod_commands, commands_control})
+
+local event_handler
+if script.active_mods["zk-lib"] then
+	event_handler = require("__zk-lib__/static-libs/lualibs/event_handler_vZO.lua")
+else
+	event_handler = require("event_handler")
+end
+event_handler.add_libraries({mod_commands, commands_control})
